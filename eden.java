bd22fc27f680346f32fa0d7939bf9b8dc413ef6e
edenbenzikri <edenbz1998@gmail.com>
2024-01-30 18:00:18 +0200

    change again

--- eden.java
+++ eden.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("My name is Eden Ben Zikri.." + i);
+            System.out.println("My name is Eden Ben Zikri!" + i);
         }
     }
 }
