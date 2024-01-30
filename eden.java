4947001eef9b544d1becd05218ae58c638dcb4e9
edenbenzikri <edenbz1998@gmail.com>
2024-01-30 18:16:22 +0200

    class name change

--- eden.java
+++ eden.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class Eden {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("My name is Eden Ben Zikri!" + i);
